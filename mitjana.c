25e12ca09c9d72a20a26f3f7cd8adaef3b9d2a30
elloma2021daw2 <elloma2021daw2@protonmail.com>
2020-12-16 21:13:22 +0100

    Segon commit del projecte mitjana

--- mitjana.c
+++ mitjana.c
@@ -6,16 +6,16 @@ int main() {
 	float nou_valor=0.0;
 	int numero_repeticions=0;
 	char consulta='y';
-	
+
 	while(consulta=='y') {
 		printf("Dòna la següent dada: ");
 		scanf("%f",&nou_valor);
 		suma=suma+nou_valor;
 		numero_repeticions=numero_repeticions+1;
 		printf("Vols continuar (y/n): ");
-		scanf(" %c",&consulta); 
+		scanf(" %s",&consulta); 
 	}
 	mitja=suma/numero_repeticions;
-	printf("La mitjana resultant és: %.2f\n",mitja);	
+	printf("La mitjana resultant és: %.2f\n",mitja);
 	return 0;
 }
